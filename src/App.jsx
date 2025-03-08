30197fe8b53274c99fe3ad2f9b6d0fc313fc9486
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-03-08 17:21:08 -0500

    check for winner move board to center

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export default function Board() {
     const nextSquares = squares.slice()
     setSquares(nextSquares)
     
-    if (xIsNext) {
+    if (xIsNext || calculateWinner(squares)) {
       nextSquares[i] = "X"
     } else {
       nextSquares[i] = "O"
@@ -51,12 +51,19 @@ export default function Board() {
     setXIsNext(!xIsNext)
   }
 
-  
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = "Winner: " + winner 
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O")
+  }
 
   // pass it a function rather than a call so it does not execute right away but on click
   // use of state and value
   return (
     <>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -76,4 +83,27 @@ export default function Board() {
   )
 }
 
+// create winning arrays stor in array, check each win array,
+// // if the first letter in array not null and same as b and c element
+// return a, the letter that won the row else return null
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null
+}
